ad323b802c06e73fe3d12293562f9145873df5df
Neha Goje <Neha.Goje@dell.com>
2022-03-01 17:13:38 +0530

    Updated Home pages and font

---
+++ Assets/js/Testimonials.js
@@ -0,0 +1,120 @@
+function testimonialDisplay1() {
+  document.getElementById('testimonial-text').innerHTML =
+    "Angaros Group's slogan is Think Big, Start Small. They really live this motto. They are very professional in their approach. They understand the needs of start-ups well and are offering us to help on digital marketing as well. They are very fair to deal with and create solutions that are win-win for both parties.";
+  document.getElementById('testimonial-img').src =
+    'https://mdbcdn.b-cdn.net/wp-content/themes/mdbootstrap4/content/en/tools/admin/reviews/avatars/small/Gerard Sanderson.jpg';
+  document.getElementById('testimonial-name').innerHTML = 'Suresh V Jayanthi';
+  document.getElementById('testimonial-position').innerHTML =
+    'Founder, Artjini Pvt Ltd, India';
+}
+
+function testimonialDisplay2() {
+  document.getElementById('testimonial-text').innerHTML =
+    'Aashish has been invaluable in helping Bubble Gum Interactive grow from an early stage business to a rapidly growing startup. He is always available to offer the breadth and depth of experience in helping our key decision making. His strong network has also been very important with a recent signed major distribution deal having been initiated firstly by his team via their contacts.';
+  document.getElementById('testimonial-img').src =
+    'https://mdbcdn.b-cdn.net/wp-content/themes/mdbootstrap4/content/en/tools/admin/reviews/avatars/small/Gerard Sanderson.jpg';
+  document.getElementById('testimonial-name').innerHTML = 'Paul Gray';
+  document.getElementById('testimonial-position').innerHTML =
+    'Co-Founder and CMO, Bubble Gum Interactive, Sydney, Australia';
+}
+
+function testimonialDisplay3() {
+  document.getElementById('testimonial-text').innerHTML =
+    "I can't speak highly enough of Aashish and his investment team, who have not only provided financial support by participating in both our capital raising rounds, but important insights and counsel during our journey as a digital start-up. Aashish and team have helped the executive team align our strategies, generate momentum and consolidate trade partner attachments by sharing their expertise and opening up his network of trade partners and investors.";
+  document.getElementById('testimonial-img').src =
+    'https://mdbcdn.b-cdn.net/wp-content/themes/mdbootstrap4/content/en/tools/admin/reviews/avatars/small/Gerard Sanderson.jpg';
+  document.getElementById('testimonial-name').innerHTML = 'Phil Mason';
+  document.getElementById('testimonial-position').innerHTML =
+    'Founder and CEO, Bubble Gum Interactive, Sydney, Australia';
+}
+
+function testimonialDisplay4() {
+  document.getElementById('testimonial-text').innerHTML =
+    'Aashish has helped us take a fresh look at our business requirements as a startup. He backup suggestions with actionable things and helped us become sustainable.';
+  document.getElementById('testimonial-img').src =
+    'https://mdbcdn.b-cdn.net/wp-content/themes/mdbootstrap4/content/en/tools/admin/reviews/avatars/small/Gerard Sanderson.jpg';
+  document.getElementById('testimonial-name').innerHTML = 'Rishi Yadati';
+  document.getElementById('testimonial-position').innerHTML =
+    'Co-founder, Gifting Inc, Delaware, USA';
+}
+
+function testimonialDisplay5() {
+  document.getElementById('testimonial-text').innerHTML =
+    'Aashish is very professional, easy to work with and more than anything else - willing to roll up their sleeves and help in anything that a start-up needs in early days. He has been of immense help in introductions to partners, new hires and zillion other things that my start-up needed in initial days.';
+  document.getElementById('testimonial-img').src =
+    'https://mdbcdn.b-cdn.net/wp-content/themes/mdbootstrap4/content/en/tools/admin/reviews/avatars/small/Gerard Sanderson.jpg';
+  document.getElementById('testimonial-name').innerHTML =
+    'Chandrakiran Mallarapu';
+  document.getElementById('testimonial-position').innerHTML =
+    'Founder, Shara eSolutions Pvt Ltd, India';
+}
+
+function testimonialDisplay6() {
+  document.getElementById('testimonial-text').innerHTML =
+    'Aashish has a very jolly personality that makes working with him easy and comfortable. Always ready to find an amicable solution to situations his leadership and decision-making skills have led us to strike balanced deals. Managing a large conglomerate of companies is something that he does with panache and professionalism. I would like to recommend Aashish to anyone looking for a mutually beneficial partnership and business engagement.';
+  document.getElementById('testimonial-img').src =
+    'https://mdbcdn.b-cdn.net/wp-content/themes/mdbootstrap4/content/en/tools/admin/reviews/avatars/small/Gerard Sanderson.jpg';
+  document.getElementById('testimonial-name').innerHTML = 'Suprit Patra';
+  document.getElementById('testimonial-position').innerHTML =
+    'Co-Founder at Gleecus, India';
+}
+
+function testimonialDisplay7() {
+  document.getElementById('testimonial-text').innerHTML =
+    'Aashish is a wonderful person to work with. He is good at digital marketing, branding and SEO and has a brilliant mind to grasp any concept quickly and to focus on what matters. He is a respected leader, a pioneer in his field and an inspiration and mentor to his staff. I highly recommend Aashish for any type of collaboration.';
+  document.getElementById('testimonial-img').src =
+    'https://mdbcdn.b-cdn.net/wp-content/themes/mdbootstrap4/content/en/tools/admin/reviews/avatars/small/Gerard Sanderson.jpg';
+  document.getElementById('testimonial-name').innerHTML = 'Roberto Mariani';
+  document.getElementById('testimonial-position').innerHTML =
+    'Founder | CEO | CTO at Menulance, Singapore';
+}
+
+function testimonialDisplay8() {
+  document.getElementById('testimonial-text').innerHTML =
+    'I know Aashish since 2005. He is the enthusiastic and driven leader I have ever seen or met. He not only has the expertise in digital marketing but, also has a strong desire to excel. I guess this is what has helped in his entrepreneurial journey. He always shows inspiration, leadership, charisma and tenacity. Through his vision and determination, he has achieved a level of recognition among his peers that many can only dream about. I strongly recommend Aashish as a business partner and mentor.';
+  document.getElementById('testimonial-img').src =
+    'https://mdbcdn.b-cdn.net/wp-content/themes/mdbootstrap4/content/en/tools/admin/reviews/avatars/small/Gerard Sanderson.jpg';
+  document.getElementById('testimonial-name').innerHTML = 'Anil Thapa';
+  document.getElementById('testimonial-position').innerHTML =
+    'AVP  at Capillary Technologies';
+}
+
+function testimonialDisplay9() {
+  document.getElementById('testimonial-text').innerHTML =
+    'Aashish was our company expert for Search Engine Optimization. His SEO results were extremely favourable, always exceeding our business goals. He is an excellent team player who works very well with others. He is a pleasure to work with. Aashish produces great results!';
+  document.getElementById('testimonial-img').src =
+    'https://mdbcdn.b-cdn.net/wp-content/themes/mdbootstrap4/content/en/tools/admin/reviews/avatars/small/Gerard Sanderson.jpg';
+  document.getElementById('testimonial-name').innerHTML = 'Brad Crain';
+  document.getElementById('testimonial-position').innerHTML =
+    'CTO at startup.com, Greater Seattle';
+}
+
+function testimonialDisplay10() {
+  document.getElementById('testimonial-text').innerHTML =
+    'Aashish was interfacing with our clients. He was the single point of contact for all our software development clients. He used to stretch himself to coordinate with clients in their timings. He is adept at project management and has good knowledge of various things related to Online Marketing and SEO.';
+  document.getElementById('testimonial-img').src =
+    'https://mdbcdn.b-cdn.net/wp-content/themes/mdbootstrap4/content/en/tools/admin/reviews/avatars/small/Gerard Sanderson.jpg';
+  document.getElementById('testimonial-name').innerHTML = 'Rama Raju R';
+  document.getElementById('testimonial-position').innerHTML =
+    'CEO at GAP Miners India';
+}
+
+function testimonialDisplay11() {
+  document.getElementById('testimonial-text').innerHTML =
+    "Angaros Group's slogan is Think Big, Start Small. They really live this motto. They are very professional in their approach. They understand the needs of start-ups well and are offering us to help on digital marketing as well. They are very fair to deal with and create solutions that are win-win for both parties.";
+  document.getElementById('testimonial-img').src =
+    'https://mdbcdn.b-cdn.net/wp-content/themes/mdbootstrap4/content/en/tools/admin/reviews/avatars/small/Gerard Sanderson.jpg';
+  document.getElementById('testimonial-name').innerHTML = 'Amogh Malve';
+  document.getElementById('testimonial-position').innerHTML =
+    'Founder, Artjini Pvt Ltd, India';
+}
+
+function testimonialDisplay12() {
+  document.getElementById('testimonial-text').innerHTML =
+    "Angaros Group's slogan is Think Big, Start Small. They really live this motto. They are very professional in their approach. They understand the needs of start-ups well and are offering us to help on digital marketing as well. They are very fair to deal with and create solutions that are win-win for both parties.";
+  document.getElementById('testimonial-img').src =
+    'https://mdbcdn.b-cdn.net/wp-content/themes/mdbootstrap4/content/en/tools/admin/reviews/avatars/small/Gerard Sanderson.jpg';
+  document.getElementById('testimonial-name').innerHTML = 'Amogh Malve';
+  document.getElementById('testimonial-position').innerHTML =
+    'Founder, Artjini Pvt Ltd, India';
+}
